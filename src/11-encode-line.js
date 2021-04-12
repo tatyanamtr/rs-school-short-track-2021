a03c4f1b86199f4d8ec9cdf6709199cdc42671ed
tatyanamtr <tatyana.a.mtr@gmail.com>
2021-04-12 19:47:44 +0300

    feat: resolve encode line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let count = 1;
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      count++;
+    } else if (count !== 1) {
+      result.push(count);
+      result.push(arr[i]);
+      count = 1;
+    } else {
+      result.push(arr[i]);
+      count = 1;
+    }
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;
