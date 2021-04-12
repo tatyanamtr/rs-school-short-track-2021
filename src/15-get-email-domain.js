cfead47826915d1c94dedbda4238b0239cc9dd30
tatyanamtr <tatyana.a.mtr@gmail.com>
2021-04-12 06:38:34 +0300

    feat: resolve get email domain

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,9 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const n = email.lastIndexOf('@');
+  return email.slice(n + 1, email.length);
 }
 
 module.exports = getEmailDomain;
