067b49cc7cb16564a067b5eb207f4858c26939fd
tatyanamtr <tatyana.a.mtr@gmail.com>
2021-04-12 20:02:55 +0300

    feat: resolve sort by height

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,33 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const newarr = [];
+  const result = [];
+  let n = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== -1) {
+      newarr.push(arr[i]);
+    }
+  }
+  for (const i of newarr) {
+    for (let j = 0; j < newarr.length; j++) {
+      if (newarr[j] > newarr[j + 1]) {
+        newarr[j] += newarr[j + 1];
+        newarr[j + 1] = newarr[j] - newarr[j + 1];
+        newarr[j] -= newarr[j + 1];
+      }
+    }
+  }
+  for (const i of arr) {
+    if (i === -1) {
+      result.push(-1);
+    } else {
+      result.push(newarr[n]);
+      n++;
+    }
+  }
+  return result;
 }
 
 module.exports = sortByHeight;
